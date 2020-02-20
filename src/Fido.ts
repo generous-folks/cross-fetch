fe1e77d329d93158773440caff19136f6797797c
hpierre74 <phuyghe.externe@m6.fr>
2020-02-20 20:34:31 +0100

    all good motha fucka

--- src/Fido.ts
+++ src/Fido.ts
@@ -1,12 +1,6 @@
 
 export * from './Fido_sources.gen';
-export { getCaca } from './web/FidoJs.gen';
+export { requestGet } from './web/FidoJs.gen';
 
 // export {
-//   sinkT as Sink,
-//   sourceT as Source,
-//   operatorT as Operator,
-//   subscriptionT as Subscription,
-//   observerT as Observer,
-//   subjectT as Subject
 // } from './Fido_types.gen';
